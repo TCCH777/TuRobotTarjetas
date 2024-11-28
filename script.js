61bb28d3e5b62009130a3d7b74e655c17e7d5b5f
italo <italocianci00@gmail.como>
2024-11-28 16:28:40 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+let database = [
+  {
+    nombre: "Co-ram",
+    tamaño: "pequeño",
+    imagen:
+      "https://firebasestorage.googleapis.com/v0/b/bielo20.appspot.com/o/robots%2Fcoram.png?alt=media&token=fbdd4648-3b9d-4bad-9c6d-6b2bd4a4e1e3",
+    modelo: "amx",
+    material: "fibra de vidrio",
+    descripcion: "",
+    energia: "electrico",
+  },
+  {
+    nombre: "Aur-oi",
+    tamaño: "grande",
+    imagen:
+      "https://firebasestorage.googleapis.com/v0/b/bielo20.appspot.com/o/robots%2Fauroi.png?alt=media&token=9a59378c-f277-4edd-a8e1-bb105fc5bf72",
+    modelo: "amx",
+    material: "fibra de vidrio",
+    descripcion: "",
+    energia: "electrico",
+  },
+  {
+    nombre: "S4-MUR4",
+    tamaño: "grande",
+    imagen:
+      "https://firebasestorage.googleapis.com/v0/b/bielo20.appspot.com/o/robots%2Fsamurai.png?alt=media&token=57762847-740d-4108-ae2f-506963b574c4",
+    modelo: "grx",
+    material: "hierro fundido",
+    descripcion: "",
+    energia: "gasolina",
+  },
+  {
+    nombre: "M1-L4GR-0",
+    tamaño: "pequeño",
+    imagen:
+      "https://firebasestorage.googleapis.com/v0/b/bielo20.appspot.com/o/robots%2Fmilagro.png?alt=media&token=1da65378-4390-4c10-ba7b-40752ecf7598",
+    modelo: "grx",
+    material: "hierro fundido",
+    descripcion: "",
+    energia: "gasolina",
+  },
+  {
+    nombre: "5UM-4QU1",
+    tamaño: "pequeño",
+    imagen:
+      "https://firebasestorage.googleapis.com/v0/b/bielo20.appspot.com/o/robots%2Fsumaq.png?alt=media&token=2dfdfb85-4da8-4fdf-bf65-a7aaf422e8f8",
+    modelo: "asx",
+    material: "asero inoxidable",
+    descripcion: "",
+    energia: "electrico",
+  },
+  {
+    nombre: "3L1-54",
+    tamaño: "grande",
+    imagen:
+      "https://firebasestorage.googleapis.com/v0/b/bielo20.appspot.com/o/robots%2Fela.png?alt=media&token=ca326804-12e7-418a-9f83-82e41e0092ad",
+    modelo: "asx",
+    material: "asero inoxidable",
+    descripcion: "",
+    energia: "nuclear",
+  },
+  {
+    nombre: "4R73",
+    tamaño: "pequeño",
+    imagen:
+      "https://firebasestorage.googleapis.com/v0/b/bielo20.appspot.com/o/robots%2Farte.png?alt=media&token=55cd59e7-0d3f-4d01-8706-b5fd1d4e9ec9",
+    modelo: "adx",
+    material: "oro",
+    descripcion: "",
+    energia: "elecrtico",
+  },
+  {
+    nombre: "713-MP0",
+    tamaño: "grande",
+    imagen:
+      "https://firebasestorage.googleapis.com/v0/b/bielo20.appspot.com/o/robots%2Ftiempo.png?alt=media&token=e97fe895-b627-4b06-8364-60236c054bac",
+    modelo: "adx",
+    material: "oro",
+    descripcion: "",
+    energia: "nuclear",
+  },
+];
